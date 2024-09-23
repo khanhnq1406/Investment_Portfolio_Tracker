86ab72840aafc4e7d0eadd276954ec825333b591
Quốc Khánh Nguyễn <khanhnq1406@gmail.com>
2024-09-23 18:53:23 +0700

    [Fix] Change socket.io host/port for deployment
    Because of error while hosting on Render.
    Try changing socket port/port to fix issue.
    
    Signed-off-by: Quốc Khánh Nguyễn <khanhnq1406@gmail.com>

--- src/server/utils/constants.js
+++ src/server/utils/constants.js
@@ -46,5 +46,5 @@ exports.REDIS_URL = REDIS_URL;
 const SOCKET =
   process.env.NODE_ENV === "development"
     ? `localhost:3000`
-    : `${process.env.RENDER_HOST}`;
+    : `${process.env.CLIENT_HOST}`;
 exports.SOCKET = SOCKET;
